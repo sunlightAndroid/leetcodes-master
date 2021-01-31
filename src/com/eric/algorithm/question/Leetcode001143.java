afa8823012c261d9ccf01f1df425ff9b067d63d4
戈传光 <1944633835@qq.com>
2021-01-31 14:47:02 +0800

    修改一下

--- src/com/eric/algorithm/question/Leetcode001143.java
+++ src/com/eric/algorithm/question/Leetcode001143.java
@@ -6,14 +6,14 @@ package com.eric.algorithm.question;
 public class Leetcode001143 {
 
     public int longestCommonSubsequence(String text1, String text2) {
-        if(text1.isEmpty() || text2.isEmpty()) return 0;
+        if (text1.isEmpty() || text2.isEmpty()) return 0;
         int m = text1.length() + 1;
         int n = text2.length() + 1;
         int[][] dp = new int[m][n];
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + 1;
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
