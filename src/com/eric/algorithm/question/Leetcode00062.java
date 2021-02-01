4ad7588fb540da233f8ea11e98671174faab004d
戈传光 <>
2021-02-01 14:45:37 +0800

    更新 62. 不同路径 64. 最小路径和 二维动态规划问题

--- src/com/eric/algorithm/question/Leetcode00062.java
+++ src/com/eric/algorithm/question/Leetcode00062.java
@@ -1,22 +1,20 @@
 package com.eric.algorithm.question;
 
-public class Leetcode00064 {
+public class Leetcode00062 {
 
-    public int minPathSum(int[][] grid) {
-        if(grid ==null || grid.length == 0) return 0;
-        int m = grid.length;
-        int n= grid[0].length;
+    public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        dp[0][0] = grid[0][0];
+        dp[0][0] = 1;
+
         for (int i = 1; i < m; i++) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
+            dp[i][0] = 1;
         }
         for (int j = 1; j < n; j++) {
-            dp[0][j] = dp[0][j-1] + grid[0][j];
+            dp[0][j] = 1;
         }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = Math.min(dp[i][j-1], dp[i-1][j]) + grid[i][j];
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
             }
         }
         return dp[m-1][n-1];
@@ -25,7 +23,7 @@ public class Leetcode00064 {
 
     public static class Test {
         public static void main(String[] args) {
-            Leetcode00064 solution = new Leetcode00064();
+            Leetcode00062 solution = new Leetcode00062();
 
 //            int result = solution.maxProduct(nums1);
 //            System.out.println(result);
